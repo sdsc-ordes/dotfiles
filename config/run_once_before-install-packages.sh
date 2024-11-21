b31ba85445b448e55727e062042f39f8f2ae9a01
Dotfiles-Work <Dotfiles@work.ch>
2024-11-21 15:07:07 +0100

    fix: setup for macOS

--- config/run_once_before-install-packages.sh
+++ config/run_once_before-install-packages.sh
@@ -7,8 +7,16 @@ function install_ubuntu_packages() {
     echo "TODO: Add here some installation for ubuntu packages."
 }
 
-if [ "$CHEZMOI_OS_RELEASE_ID" = "ubuntu" ]; then
+function install_macos_packages() {
+    echo "TODO: Add here some installation for macOS packages."
+
+    brew install git git-lfs coreutils findutils binutils gnu-sed gnu-tar gnu-time grep
+}
+
+if [ "$CHEZMOI_OS" = "darwin" ] ; then
+    install_macos_packages
+elif [ "$CHEZMOI_OS_RELEASE_ID" = "ubuntu" ]; then
     install_ubuntu_packages
 else
-    echo "WARNING: Architecture '$CHEZMOI_OS_RELEASE_ID' not supported for installing packages." >&2
+    echo "WARNING: Architecture '$CHEZMOI_OS' not supported for installing packages." >&2
 fi
